5e6729f05f120b0ed5dfe17d5a219f20a7967c44
Jeff King <jeffking@gmail.com>
2021-03-09 05:11:48 -0800

    Add card for repo disabled yet zero results.

--- docs/dist/app.js
+++ docs/dist/app.js
@@ -154,7 +154,7 @@ export function App() {
   }, (() => {
     if (!isRespository)
       return null;
-    if (!resultCount)
+    if (repoEnabled && !resultCount)
       return null;
     return /* @__PURE__ */ React.createElement(Page, {
       className: "heightAuto bolt-page-grey"
@@ -173,15 +173,16 @@ export function App() {
       iconName: "Lock",
       size: IconSize.large
     }), /* @__PURE__ */ React.createElement("div", {
+      className: "flex-grow",
       style: {margin: "0 32px 0 16px"}
-    }, /* @__PURE__ */ React.createElement("div", null, "The '", repository ?? repo, "' repository has been disabled because it contains live secrets in its source code or history. All repositories inside Microsoft must be free of secrets. You may temporarily enable this repository by clicking the 'Enable Repository' button. Your identity will be associated with this request."), /* @__PURE__ */ React.createElement("div", {
+    }, !resultCount ? /* @__PURE__ */ React.createElement("div", null, "No live secrets were detected. This repository is currently disabled (due to system refresh latency or missing repository ownership information). Click 'Enable Repository' to enable the repository.") : /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("div", null, "The '", repository ?? repo, "' repository has been disabled because it contains live secrets in its source code or history. All repositories inside Microsoft must be free of secrets. You may temporarily enable this repository by clicking the 'Enable Repository' button. Your identity will be associated with this request."), /* @__PURE__ */ React.createElement("div", {
       style: {marginTop: 12}
     }, /* @__PURE__ */ React.createElement(Checkbox, {
       label: "I understand that by enabling this repository, I accept responsibility to ensure all currently exposed secrets are invalidated within 72 hours.",
       checked: responsibility,
       onChange: (_, checked) => setResponsibility(checked)
-    }))), /* @__PURE__ */ React.createElement(Button, {
-      disabled: !responsibility,
+    })))), /* @__PURE__ */ React.createElement(Button, {
+      disabled: !!resultCount && !responsibility,
       onClick: async () => {
         try {
           const response = await fetchSpam("enable", "PATCH");
